f622a15d67104d0702f2ef2e3c38d2f6dab857c3
Blake Barbour <blaketbarbour@gmail.com>
2025-11-09 19:49:46 -0500

    adding structure

--- testing/testAPIKey.py
+++ testing/testAPIKey.py
@@ -9,5 +9,9 @@ system = "1214"          # pick a valid system id
 outdir = "data"          # will be created if missing
 file_type = "csv"        # or "parquet"
 
+# Download from this directory; 
+#   https://data.openei.org/s3_viewer?bucket=oedi-data-lake&prefix=pvdaq%2Fcsv%2F
+# - Use .csv files for consistency
+# - 
 pvdaq_access.downloadData(system, outdir, file_type=file_type)
 print("done")
---
+++ testing/verifyAccess.py
@@ -0,0 +1,2 @@
+import pvdaq_access
+pvdaq_access.getSystems()[:5]  # lists accessible IDs
